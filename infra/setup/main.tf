d7a338627ef33efd52adb48a4be4640c58bcbe2b
Joel G <joel@joelgonzaga.com>
2025-04-12 10:19:26 -0700

    formated some files because it makes it easier to debug

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -1,28 +1,28 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "5.23.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
     }
+  }
 
-    backend "s3" {
-        bucket = "jtg-terraform-buckets"
-        key = "tf-state-setup"
-        region = "us-west-2"
-        encrypt = true
-        dynamodb_table = "terraform-lock-table"
-    }
+  backend "s3" {
+    bucket         = "jtg-terraform-buckets"
+    key            = "tf-state-setup"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
 }
 
 provider "aws" {
-    region = "us-west-2"
-    default_tags {
-      tags = {
-        Environment = teraform.workspace
-        Project = var.project
-        contact = var.contact
-        ManageBy = "Terraform/setup"
-      }
+  region = "us-west-2"
+  default_tags {
+    tags = {
+      Environment = teraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
     }
+  }
 }
--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,19 +1,19 @@
 variable "tf_state_bucket" {
-    description = "Name of an s3 bucket that stores the Terraform state"
-    default = "jtg-terraform-buckets"
+  description = "Name of an s3 bucket that stores the Terraform state"
+  default     = "jtg-terraform-buckets"
 }
 
-variable tf_state_lock_table {
-    description = "The DynamoDB table handles Terraform locks"
-    default = "terraform-lock-table"
+variable "tf_state_lock_table" {
+  description = "The DynamoDB table handles Terraform locks"
+  default     = "terraform-lock-table"
 }
 
 variable "project" {
-    description = "This be the name of the project, yarg"
-    default = "terrform-rest-api"
+  description = "This be the name of the project, yarg"
+  default     = "terrform-rest-api"
 }
 
 variable "contact" {
-    description = "who to contact about these resources"
-    default = "joel@joelgonzaga.com"
+  description = "who to contact about these resources"
+  default     = "joel@joelgonzaga.com"
 }
