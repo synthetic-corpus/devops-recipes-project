7260eff52b525340bf93d196d54464fbe779d878
Joel G <joel@joelgonzaga.com>
2025-04-12 10:58:39 -0700

    prepped the deploy tf files.

---
+++ infra/deploy/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket               = "jtg-terraform-buckets"
+    key                  = "tf-state-deploy"
+    workspace_key_prefix = "tf-state-deploy-env"
+    region               = "us-west-2"
+    encrypt              = true
+    dynamodb_table       = "terraform-lock-table"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  default_tags {
+    tags = {
+      Environment = teraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/deploy"
+    }
+  }
+}
+
+locals {
+  prefix = "${var.prefix}=${terraform.workspace}"
+}
+
+data "aws_region" "current" {}
---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for resoruces in AWS"
+  default     = "tra"
+}
+
+variable "project" {
+  description = "This be the name of the project, yarg"
+  default     = "terrform-rest-api"
+}
+
+variable "contact" {
+  description = "who to contact about these resources"
+  default     = "joel@joelgonzaga.com"
+}
