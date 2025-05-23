7a1cc08ff0532042fabf6f6cbe4dbed52b9fc207
Joel G <joel@joelgonzaga.com>
2025-05-23 15:53:01 -0700

    added the proxy

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -22,7 +22,14 @@ variable "db_password" {
   description = "Password for the terraform database."
 }
 
-
 variable "ecr_proxy_image" {
   description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the image image"
+}
+
+variable "django_secret_key" {
+  description = "Django secret key (from git hub actions)"
 }
