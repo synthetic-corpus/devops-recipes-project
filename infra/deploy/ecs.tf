d6f51d8b283b5aba41ba98d5196e2abebc5c87c9
Joel G <joel@joelgonzaga.com>
2025-05-23 17:40:00 -0700

    fixed a format.

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -164,32 +164,32 @@ resource "aws_ecs_task_definition" "api" {
 
 resource "aws_security_group" "ecs_service" {
   description = "outgoing rules for ECS"
-  name = "${local.prefix}-ecs-service"
-  vpc_id = aws_vpc.main.id
+  name        = "${local.prefix}-ecs-service"
+  vpc_id      = aws_vpc.main.id
 
   # Access to end points
   egress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Access to RDS
   egress {
     from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
+    to_port   = 5432
+    protocol  = "tcp"
     cidr_blocks = [
-        aws_subnet.private_a.cidr_block,
-        aws_subnet.private_b.cidr_block
-        ]
-    }
-    # Inbound access from internet
-    ingress {
-        from_port = 8000
-        to_port = 8000
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block
+    ]
   }
+  # Inbound access from internet
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
