0d1552bb747d2051e89e4c8996c2ce1dbd9d7489
Joel G <joel@joelgonzaga.com>
2025-05-30 16:31:28 -0700

    added the corresponding efs access for the proxy.

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -101,6 +101,11 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = false
             containerPath = "/vol/web/static"
             sourceVolume  = "static"
+          },
+          {
+            readOnly      = false
+            containerPath = "/vol/web/media"
+            sourceVolume  = "efs-media"
           }
         ],
         logConfiguration = {
@@ -135,6 +140,11 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = true
             containerPath = "/vol/static"
             sourceVolume  = "static"
+          },
+          {
+            readOnly      = true
+            containerPath = "/vol/web/media"
+            sourceVolume  = "efs-media"
           }
         ]
         logConfiguration = {
